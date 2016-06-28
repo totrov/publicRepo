29550577b5044c120eacce09ed227b2e3cddf65d
Totree <totrovOV@kyrs.server>
2016-06-28 13:23:27 +0300

    Вывод клиентов на страницу ajax'ом базовый вывод, будут дорабатываться

--- TestExercise/TestExercise/Controllers/HomeController.cs
+++ TestExercise/TestExercise/Controllers/HomeController.cs
@@ -15,5 +15,10 @@ namespace TestExercise.Controllers
             }
             return View();
         }
+
+        public string GetClientsJSON(int pageNumber, int clientsPerPage)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(new List<Client> { new Client { Fio = "sdfc" } });
+        }
     }
 }
